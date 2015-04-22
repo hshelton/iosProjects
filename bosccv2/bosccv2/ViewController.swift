b8695935ddd63356e44ecf1ce503b444ccd5d8d1
hshelton <hayden.shelton@utah.edu>
2015-04-22 15:59:27 -0600

    updates made

---
+++ bosccv2/bosccv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bosccv2
+//
+//  Created by Hayden Shelton on 4/16/15.
+//  Copyright (c) 2015 Hayden Shelton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
